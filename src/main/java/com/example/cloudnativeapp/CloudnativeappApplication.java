05164df9f1ab74871f9ad334958ad2c60179c067
Shanmugapriya005 <12sha46@gmail.com>
2026-05-29 15:25:46 +0530

    Initial Spring Boot application

---
+++ src/main/java/com/example/cloudnativeapp/CloudnativeappApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudnativeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudnativeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudnativeappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cloudnativeapp/CloudnativeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudnativeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudnativeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
